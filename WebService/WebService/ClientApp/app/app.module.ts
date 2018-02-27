11f383ea716f08fdd31d2eed8c9d1c61edf3b834
Kenan Bayramoglu <info@codeplex.be>
2018-02-27 11:16:40 +0100

    up

--- WebService/WebService/ClientApp/app/app.module.ts
+++ WebService/WebService/ClientApp/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { TestingComponent } from './components/testing/testing.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        TestingComponent
     ],
     imports: [
         CommonModule,
