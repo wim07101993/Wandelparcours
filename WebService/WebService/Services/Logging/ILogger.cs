b35f610a9739a4d2f27731137989ac12ed0fcbff
wim van laer <van.laer.wim@gmail.com>
2018-02-27 12:06:40 +0100

    created loggin service

---
+++ WebService/WebService/Services/Logging/ILogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WebService.Services.Logging
+{
+    /// <summary>
+    /// Represents a type used to perform logging.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Log&lt;T&gt; is supposed to log the message of an exception to the file.
+        /// </summary>
+        /// <typeparam name="T">is the type of the sender that wants to log the exception</typeparam>
+        /// <param name="sender">is that wants to log the exception</param>
+        /// <param name="logLevel">is the importance of the log</param>
+        /// <param name="exception">is the exception to log the message of</param>
+        void Log<T>(T sender, ELogLevel logLevel, Exception exception);
+
+        /// <summary>
+        /// Log&lt;T&gt; is supposed to log a message to the file.
+        /// </summary>
+        /// <typeparam name="T">is the type of the sender that wants to log the message</typeparam>
+        /// <param name="sender">is that wants to log the exception</param>
+        /// <param name="logLevel">is the importance of the log</param>
+        /// <param name="message">is the message to log</param>
+        void Log<T>(T sender, ELogLevel logLevel, string message);
+    }
+}
