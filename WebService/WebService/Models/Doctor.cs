30ae0f01811f8abe5dd9c60227303dc0ee891c40
wim van laer <van.laer.wim@gmail.com>
2018-02-27 15:26:01 +0100

    changed resident model

---
+++ WebService/WebService/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebService.Models
+{
+    public class Doctor
+    {
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
