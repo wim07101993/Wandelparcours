3d48c6d97e2edb6e3570ef79b0234e19d00752ec
wim van laer <van.laer.wim@gmail.com>
2018-02-27 10:41:34 +0100

    created Models

---
+++ WebService/WebService/Models/EAuthLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebService.Models
+{
+    public enum EAuthLevel
+    {
+        SysAdmin = 0,
+        Admin = 1,
+        User = 2,
+        UnVerifiedUser = 3,
+        Guest = 4
+    }
+}
