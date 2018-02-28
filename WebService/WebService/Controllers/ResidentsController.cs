cc0f0c239dbd8052a71728d691dfbd854a6b2a9a
wim van laer <van.laer.wim@gmail.com>
2018-02-28 10:57:37 +0100

    rest-api POST Create Resident created

--- WebService/WebService/Controllers/ResidentsController.cs
+++ WebService/WebService/Controllers/ResidentsController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using WebService.Services.Logging;
 using MongoDB.Driver;
 using WebService.Models;
@@ -26,7 +27,7 @@ namespace WebService.Controllers
         public ResidentsController(IConfiguration config, ILogger logger)
         {
             _logger = logger;
-            
+
             _collection = new MongoClient(config["Database:ConnectionString"])
                 .GetDatabase(config["Database:DatabaseName"])
                 .GetCollection<Resident>(config["Database:ResidentsCollectionName"]);
@@ -49,11 +50,27 @@ namespace WebService.Controllers
                     .Include(x => x.Room)
                     .Include(x => x.Birthday)
                     .Include(x => x.Doctor);
-                
+
                 return Ok(_collection
                     .Find(FilterDefinition<Resident>.Empty)
                     .Project<Resident>(selector)
                     .ToList());
+            }
+            catch (Exception e)
+            {
+                _logger.Log(this, ELogLevel.Error, e);
+                return StatusCode((int) HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Resident resident)
+        {
+            try
+            {
+                resident.ID = ObjectId.GenerateNewId();
+                _collection.InsertOne(resident);
+                return StatusCode((int) HttpStatusCode.Created);
             }
             catch (Exception e)
             {
