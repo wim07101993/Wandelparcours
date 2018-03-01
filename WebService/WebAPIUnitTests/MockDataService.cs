e6ab1296e2244a1bdf9d7d3766a68843ff21784d
wim van laer <van.laer.wim@gmail.com>
2018-03-01 13:35:20 +0100

    TestProject for REST api  Created

---
+++ WebService/WebAPIUnitTests/MockDataService.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebAPIUnitTests
+{
+    [TestClass]
+    public class MockDataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
