8d40c078a05236959ef92e448cf0c841ef488b98
Kenan Bayramoglu <info@codeplex.be>
2018-03-01 15:20:49 +0100

    Revert "Lats Commit before angular material"
    This reverts commit 92a1750e677f6931bb414fff6153191b0d6cb3f6.

--- WebService/WebService/ClientApp/app/components/resident/resident.component.ts
+++ WebService/WebService/ClientApp/app/components/resident/resident.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Resident } from '../../models/resident';
+import { Resident } from '../../models/resident'
 
 @Component({
   selector: 'app-resident',
