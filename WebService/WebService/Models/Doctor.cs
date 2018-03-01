42fd97978826231c6a4e4e12b31d01b8c318a08c
wim van laer <van.laer.wim@gmail.com>
2018-03-01 11:58:27 +0100

    REST: documentation

--- WebService/WebService/Models/Doctor.cs
+++ WebService/WebService/Models/Doctor.cs
@@ -2,11 +2,24 @@
 
 namespace WebService.Models
 {
+    /// <summary>
+    /// Doctor is the representation of the doctor that takes care of the Resident.
+    /// </summary>
     public class Doctor
     {
+        /// <summary>
+        /// Name is the name of the User
+        /// <para/>
+        /// In the database the value is stored under the field "name"
+        /// </summary>
         [BsonElement("name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// PhoneNumber is the phone number of the User
+        /// <para/>
+        /// In the database the value is stored under the field "phoneNumber"
+        /// </summary>
         [BsonElement("phoneNumber")]
         public string PhoneNumber { get; set; }
 
