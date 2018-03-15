095d7adc55e927f99db5480f58f921cea713af14
KerimBayramoglu <kerim.bay91@gmail.com>
2018-03-15 09:47:23 +0100

    npm problem fix

--- WebService/WebService/ClientApp/app/components/senior/upload/upload.component.ts
+++ WebService/WebService/ClientApp/app/components/senior/upload/upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { RestServiceService } from '../../../service/rest-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -6,11 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+    id: string = this.route.snapshot.params['id'];
     @Input() type: string;
     fd: FormData = new FormData();
     selectedFile: File;
+    blob: Blob;
 
-    constructor() {}
+    constructor(private restService: RestServiceService, private route: ActivatedRoute) {}
 
   ngOnInit() {
   }
@@ -22,11 +26,12 @@ export class UploadComponent implements OnInit {
 
   async onUpload() {
       this.fd.append('images/data', this.selectedFile, this.selectedFile.name);
-      console.log(this.fd); console.log(this.selectedFile.type);
+      this.blob = new Blob()
+      console.log(this.fd);
       //let headers = new Headers();
       //let options = new RequestOptions({ headers: headers });    
       //await this.service.addImagesToDatabase(this.id, new Blob([JSON.stringify(this.selectedFile.name)], { type: "application/json"}));
-      //let images = this.service.addImagesToDatabase(this.id, this.fd);
+      let images = this.restService.addImagesToDatabase(this.id, this.fd);
       //console.log(images);
   }    
 
