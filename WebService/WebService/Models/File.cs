f4b58af7f915c9302c5b02e49718def9819a52ae
wim van laer <van.laer.wim@gmail.com>
2018-03-15 10:17:54 +0100

    added add media methods for all media types

--- WebService/WebService/Models/File.cs
+++ WebService/WebService/Models/File.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using WebService.Helpers.Exceptions;
+
+
+// TODO DOC
 
 namespace WebService.Models
 {
     public class FormFile
     {
         public IFormFile File { get; set; }
+
+        public byte[] ConvertToBytes(int maxFileSize = int.MaxValue)
+        {
+            using (var stream = File.OpenReadStream())
+            {
+                if (!stream.CanRead)
+                    throw new Exception("cannot read stream");
+
+                if (stream.Length > maxFileSize)
+                    throw new FileToLargeException(
+                        $"the file is to large it is {stream.Length} bytes and the maximum is {maxFileSize}");
+
+                var bytes = new List<byte>((int) stream.Length);
+                for (var i = 0; i < stream.Length; i++)
+                    bytes.Add((byte) stream.ReadByte());
+
+                return bytes.ToArray();
+            }
+        }
     }
 }
