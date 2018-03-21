de49a42a27e866f8b99a70fd5af8b36d294330e3
wim van laer <van.laer.wim@gmail.com>
2018-03-21 09:35:56 +0100

    made the issued tokens public in readonly

--- WebService/WebService/Services/Authorization/TokenService.cs
+++ WebService/WebService/Services/Authorization/TokenService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -13,10 +15,14 @@ namespace WebService.Services.Authorization
 {
     public class TokenService : ITokenService
     {
+        private const int TokenLifeTime = 30;
+
         private readonly IConfiguration _configuration;
         private readonly IUsersService _usersService;
         private readonly IThrow _iThrow;
         private readonly ILogger _logger;
+        private readonly List<string> _issuedTokens = new List<string>();
+
 
         public TokenService(IConfiguration configuration, IUsersService usersService, IThrow iThrow,
             ILogger logger)
@@ -25,7 +31,13 @@ namespace WebService.Services.Authorization
             _usersService = usersService;
             _iThrow = iThrow;
             _logger = logger;
+
+            RunTokenGarbageCollectorAsync();
         }
+
+        public IReadOnlyList<string> IssuedTokens => _issuedTokens;
+
+        public bool IsTokenGarbageCollectorRunning { get; set; }
 
 
         public async Task<string> CreateTokenAsync(string userName, string password)
@@ -39,10 +51,11 @@ namespace WebService.Services.Authorization
             var token = new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
                 issuer: _configuration["Jwt:Issuer"],
                 audience: _configuration["Jwt:Issuer"],
-                expires: DateTime.Now.AddMinutes(30),
+                expires: DateTime.Now.AddMinutes(TokenLifeTime),
                 signingCredentials: creds
             ));
 
+            _issuedTokens.Add(token);
             _logger.Log(this, ELogLevel.Information, $"Issued new token: {token}");
             return token;
         }
@@ -65,10 +78,27 @@ namespace WebService.Services.Authorization
             }
             catch
             {
-                // IGNORED
+                return false;
+            }
+        }
+
+        private async void RunTokenGarbageCollectorAsync()
+        {
+            IsTokenGarbageCollectorRunning = true;
+
+            while (IsTokenGarbageCollectorRunning)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(TokenLifeTime));
+
+                if (!_issuedTokens.Any())
+                    continue;
+
+                for (var i = _issuedTokens.Count - 1; i >= 0; i--)
+                    if (!ValidateToken(_issuedTokens[i]))
+                        _issuedTokens.RemoveAt(i);
             }
 
-            return false;
+            IsTokenGarbageCollectorRunning = false;
         }
     }
 }
