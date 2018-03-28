becba6cd68e4fdbbc023ee239a6e6fc67996e2fc
sanlihakan <sanlihakan@outlook.com>
2018-03-28 15:16:52 +0200

    create global tracking

---
+++ WebService/WebService/ClientApp/app/components/globaltracking/globaltracking.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GlobaltrackingComponent } from './globaltracking.component';
+
+describe('GlobaltrackingComponent', () => {
+  let component: GlobaltrackingComponent;
+  let fixture: ComponentFixture<GlobaltrackingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GlobaltrackingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GlobaltrackingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
