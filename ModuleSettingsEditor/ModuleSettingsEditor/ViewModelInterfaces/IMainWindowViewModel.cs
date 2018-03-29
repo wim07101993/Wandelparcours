5d5cdfb1f1f83260929fd0afd11648ed4f28db35
wim van laer <van.laer.wim@gmail.com>
2018-03-29 21:24:28 +0200

    view first

---
+++ ModuleSettingsEditor/ModuleSettingsEditor/ViewModelInterfaces/IMainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using ModuleSettingsEditor.Models;
+
+namespace ModuleSettingsEditor.ViewModelInterfaces
+{
+    public interface IMainWindowViewModel
+    {
+        Settings Settings { get; set; }
+        IDictionary<string, object> Properties { get; }
+
+        ICommand SaveCommand { get; }
+        ICommand OpenCommand { get; }
+    }
+}
