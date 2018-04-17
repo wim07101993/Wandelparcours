6b8d1a3a7b56ed5361d00549a9157f1382533f80
Wim Van Laer <van.laer.wim@gmail.com>
2018-04-17 16:08:40 +0200

    middleware

--- WebService/WebService/Middleware/AuthPipelineFilter.cs
+++ WebService/WebService/Middleware/AuthPipelineFilter.cs
@@ -9,10 +9,8 @@ using WebService.Controllers.Bases;
 using WebService.Helpers.Attributes;
 using WebService.Helpers.Exceptions;
 using WebService.Helpers.Extensions;
-using WebService.Models;
 using WebService.Services.Authorization;
 using WebService.Services.Data;
-using WebService.Services.Data.Mongo;
 
 namespace WebService.Middleware
 {
@@ -21,12 +19,14 @@ namespace WebService.Middleware
         private readonly ITokenService _tokenService;
         private readonly IUsersService _usersService;
 
+        
         public AuthPipelineFilter(ITokenService tokenService, IUsersService usersService)
         {
             _tokenService = tokenService;
             _usersService = usersService;
         }
 
+        
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (!(context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor))
--- WebService/WebService/Middleware/ExceptionMiddleware.cs
+++ WebService/WebService/Middleware/ExceptionMiddleware.cs
@@ -24,56 +24,36 @@ namespace WebService.Middleware
             {
                 await _next(context);
             }
-            // check for bad arguments by the client
             catch (Helpers.Exceptions.ArgumentException e)
             {
-                // respond with a 400 bad request status code
                 context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
 
-                // respond a message
-                await context.Response.WriteAsync(
-                    "The arguments you passed just destroyed Luxembourg...\n\n");
-
-                // respond the error
-                await context.Response.WriteAsync((string) e.Message);
-            }
-            // check if some recourse is not found
-            catch (NotFoundException e)
-            {
-                // respond with a 404 not found status code
-                context.Response.StatusCode = (int) HttpStatusCode.NotFound;
-
-                // respond a message
-                await context.Response.WriteAsync("These are not the droids you're looking for.\n\n");
-
-                // respond the error
+                await context.Response.WriteAsync("The arguments you passed just destroyed Luxembourg...\n\n");
                 await context.Response.WriteAsync(e.Message);
             }
-            // check for unauthorized requests
+            catch (NotFoundException e)
+            {
+                context.Response.StatusCode = (int) HttpStatusCode.NotFound;
+
+                await context.Response.WriteAsync("These are not the droids you're looking for.\n\n");
+                await context.Response.WriteAsync(e.Message);
+            }
             catch (UnauthorizedException e)
             {
-                // respond with a 404 not found status code
                 context.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
 
-                // respond a message
                 await context.Response.WriteAsync("You shall not pass!!\n\n");
-
-                // respond the error
                 await context.Response.WriteAsync(e.Message);
 
                 _logger.Log(this, ELogLevel.Information, e);
             }
-            // catch all other exceptions
             catch (Exception e)
             {
-                // respond with  500 internal server error status code
                 context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
 
-                // log the exception
-                _logger.Log(this, ELogLevel.Error, e);
+                await context.Response.WriteAsync("<img src=\"http://gph.is/1s201Ez\">");
 
-                // respond with fancy gif
-                await context.Response.WriteAsync("http://gph.is/1s201Ez");
+                _logger.Log(this, ELogLevel.Error, e);
             }
         }
     }
