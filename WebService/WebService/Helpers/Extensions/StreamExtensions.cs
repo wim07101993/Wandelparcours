9c580b4fe963845e8c4c175a42d7b7c9c01a0316
Wim Van Laer <van.laer.wim@gmail.com>
2018-05-09 16:42:38 +0200

    added video converter to add media (but needs testing)

---
+++ WebService/WebService/Helpers/Extensions/StreamExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WebService.Helpers.Exceptions;
+
+namespace WebService.Helpers.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToBytes(this Stream This, int maxFileSize = int.MaxValue)
+        {
+            using (This)
+            {
+                if (!This.CanRead)
+                    throw new Exception("cannot read stream");
+
+                if (This.Length > maxFileSize)
+                    throw new FileToLargeException(maxFileSize);
+
+                var bytes = new List<byte>((int) This.Length);
+                for (var i = 0; i < This.Length; i++)
+                    bytes.Add((byte) This.ReadByte());
+
+                return bytes.ToArray();
+            }
+        }
+    }
+}
